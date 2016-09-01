3b447a02be14b70c20e92918382955c3202a7728
Akshay S <akshay.s@birdvision.in>
2016-09-01 17:01:52 +0530

    Added unchanged application.rb file

--- config/application.rb
+++ config/application.rb
@@ -6,11 +6,11 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-config = YAML.load(File.read(File.expand_path('../application.yml', __FILE__)))
-config.merge! config.fetch(Rails.env, {})
-config.each do |key, value|
-  ENV[key] = value.to_s unless value.kind_of? Hash
-end
+# config = YAML.load(File.read(File.expand_path('../application.yml', __FILE__)))
+# config.merge! config.fetch(Rails.env, {})
+# config.each do |key, value|
+  # ENV[key] = value.to_s unless value.kind_of? Hash
+# end
 
 module App
   class Application < Rails::Application
